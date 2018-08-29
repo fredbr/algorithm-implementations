439df725dcf2077c7fdad031f7c999d92d9e3719
fredbr <fredericobsr@gmail.com>
2018-08-29 12:04:57 -0300

    Added matrix exponentiation

---
+++ math/matrix_expo.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+template <int n, int m>
+struct Matrix
+{
+	ll v[n][m];
+};
+
+ll mod = 1e9+7;
+
+template <int n, int m, int p>
+Matrix<n, p> operator*(Matrix<n, m> const& a, Matrix<m, p> const& b)
+{
+	auto ans = Matrix<n, p>();
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < p; j++)
+			for (int k = 0; k < m; k++)
+				ans.v[i][j] = (ans.v[i][j] + a.v[i][k]*b.v[k][j])%mod;
+	return ans;
+}
+
+template <int n>
+Matrix<n, n> power(Matrix<n, n> a, ll b)
+{
+	Matrix ans = a;
+	b--;
+	while (b) {
+		if (b&1) ans = ans*a;
+		a = a*a;
+		b /= 2;
+	}
+	return ans;
+}
+
+template <int n, int m>
+ostream& operator<< (ostream& out, Matrix<n, m> const& mat)
+{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++)
+			out << mat.v[i][j] << " ";
+		out << "\n";
+	}
+	return out;
+}
+
+// Muro (OBI - 2018)
+int main()
+{
+	Matrix<3, 1> base = {1, 1, 5};
+
+	ll n;
+	cin >> n;
+
+	if (n < 3) {
+		cout << base.v[0][n] << "\n";
+		return 0;
+	}
+	
+	Matrix<3,3> exp = {0, 1, 0, 
+	                   0, 0, 1,
+	                   2, 4, 1};
+
+	auto ans = power(exp, n)*base;
+
+	cout << ans.v[0][0] << "\n";
+}
