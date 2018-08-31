c9e414e8dd22ad5d62463a6d87dddb814c29bedf
fredbr <fredericobsr@gmail.com>
2018-08-31 17:20:46 -0300

    Simplified compression code

--- etc/compression.cpp
+++ etc/compression.cpp
@@ -2,33 +2,33 @@
 
 using namespace std;
 
+
+// compresses positions [0,n) to values [1, n]
 template <typename T>
 void compress(T& v, int n)
 {
-	vector<typename remove_reference<decltype(v[0])>::type> vv(n+1);
-	for (int i = 1; i <= n; i++)
+	vector<typename decay<decltype(v[0])>::type> vv(n);
+	for (int i = 0; i < n; i++)
 		vv[i] = v[i];
 
-	sort(vv.begin()+1, vv.end());
+	sort(vv.begin(), vv.end());
 
-	for (int i = 1; i <= n; i++)
-		v[i] = lower_bound(vv.begin()+1, vv.end(), v[i]) - vv.begin();
+	for (int i = 0; i < n; i++)
+		v[i] = lower_bound(vv.begin(), vv.end(), v[i]) - vv.begin() + 1;
 }
 
-int va[5] = {0, 1000, 2000, 5000, 3000}; 
-vector<int> v{0, 1000, 2000, 5000, 3000};
+int va[] = {1000, 2000, 5000, 3000}; 
+vector<int> v{1000, 2000, 5000, 3000};
 
 int main()
 {
 	compress(va, 4);
 	compress(v, 4);
 
-	for (int i = 1; i <= 4; i++)
+	for (int i = 0; i < 4; i++)
 		cout << va[i] << " ";
 	cout << "\n";
-	
-	for (int i = 1; i <= 4; i++)
-		cout << v[i] << " ";
-	cout << "\n";
 
+	for (int i : v) cout << i << " ";
+	cout << "\n";
 }
