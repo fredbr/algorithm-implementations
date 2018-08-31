c6bd742b13c65f882946988ff0068b3158d6dd55
fredbr <fredericobsr@gmail.com>
2018-08-31 17:06:27 -0300

    Generalized compression algorithm

--- etc/compression.cpp
+++ etc/compression.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 template <typename T>
-void compress(T *v, int n)
+void compress(T& v, int n)
 {
-	vector<T> vv(n+1);
+	vector<typename remove_reference<decltype(v[0])>::type> vv(n+1);
 	for (int i = 1; i <= n; i++)
 		vv[i] = v[i];
 
@@ -15,6 +15,20 @@ void compress(T *v, int n)
 		v[i] = lower_bound(vv.begin()+1, vv.end(), v[i]) - vv.begin();
 }
 
+int va[5] = {0, 1000, 2000, 5000, 3000}; 
+vector<int> v{0, 1000, 2000, 5000, 3000};
+
 int main()
 {
+	compress(va, 4);
+	compress(v, 4);
+
+	for (int i = 1; i <= 4; i++)
+		cout << va[i] << " ";
+	cout << "\n";
+	
+	for (int i = 1; i <= 4; i++)
+		cout << v[i] << " ";
+	cout << "\n";
+
 }
